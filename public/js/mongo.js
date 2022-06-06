15ccc16c5b19197bcfab4980a61ba567e1986569
Mehar <immehar25@gmail.com>
2022-06-06 23:52:08 +0530

    connected to  atlas

--- public/js/mongo.js
+++ public/js/mongo.js
@@ -2,12 +2,16 @@ const express = require("express");
 const { Db } = require("mongodb");
 const mongoose = require("mongoose");
 const validator = require("validator");
-mongoose.connect("mongodb://127.0.0.1:27017/newsletter", {
+require("dotenv").config();
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
+
+
+
 const app = express();
 app.use(express.json());
 
